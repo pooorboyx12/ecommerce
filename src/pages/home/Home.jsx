f3dd3b552dc1945b763d9684e16e3f2b2059a676
Pooorboy <pooorboyx12@gmail.com>
2023-03-01 11:22:14 +0700

    fix 4

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Featured from "../../components/Featured/Featured";
+import Featured from "../../components/featured/Featured";
 import Features from "../../components/features/Features";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
-import CartList from "../../components/CartList/CartList";
+import CartList from "../../components/cartList/CartList";
 const Home = () => {
 	return (
 		<div>
